368a9690ce9245540b8227d49a6cd15566461f63
Donny <1021001390@qq.com>
2018-03-30 11:20:46 +0800

    stage some code

---
+++ src/server/public/js/main.js
@@ -0,0 +1,194 @@
+/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+
+'use strict'
+
+var videoElement = document.querySelector('video')
+var audioInputSelect = document.querySelector('select#audioSource')
+var audioOutputSelect = document.querySelector('select#audioOutput')
+var videoSelect = document.querySelector('select#videoSource')
+var selectors = [audioInputSelect, audioOutputSelect, videoSelect]
+var mediaRecorder
+var recordedBlobs = []
+
+audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype)
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  var values = selectors.map(function(select) {
+    return select.value
+  })
+  selectors.forEach(function(select) {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild)
+    }
+  })
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i]
+    var option = document.createElement('option')
+    option.value = deviceInfo.deviceId
+    if (deviceInfo.kind === 'audioinput') {
+      option.text =
+        deviceInfo.label || 'microphone ' + (audioInputSelect.length + 1)
+      audioInputSelect.appendChild(option)
+    } else if (deviceInfo.kind === 'audiooutput') {
+      option.text =
+        deviceInfo.label || 'speaker ' + (audioOutputSelect.length + 1)
+      audioOutputSelect.appendChild(option)
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1)
+      videoSelect.appendChild(option)
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo)
+    }
+  }
+  selectors.forEach(function(select, selectorIndex) {
+    if (
+      Array.prototype.slice.call(select.childNodes).some(function(n) {
+        return n.value === values[selectorIndex]
+      })
+    ) {
+      select.value = values[selectorIndex]
+    }
+  })
+}
+
+navigator.mediaDevices
+  .enumerateDevices()
+  .then(gotDevices)
+  .catch(handleError)
+
+// Attach audio output device to video element using device/sink ID.
+function attachSinkId(element, sinkId) {
+  if (typeof element.sinkId !== 'undefined') {
+    element
+      .setSinkId(sinkId)
+      .then(function() {
+        console.log('Success, audio output device attached: ' + sinkId)
+      })
+      .catch(function(error) {
+        var errorMessage = error
+        if (error.name === 'SecurityError') {
+          errorMessage =
+            'You need to use HTTPS for selecting audio output ' +
+            'device: ' +
+            error
+        }
+        console.error(errorMessage)
+        // Jump back to first output device in the list as it's the default.
+        audioOutputSelect.selectedIndex = 0
+      })
+  } else {
+    console.warn('Browser does not support output device selection.')
+  }
+}
+
+function changeAudioDestination() {
+  var audioDestination = audioOutputSelect.value
+  attachSinkId(videoElement, audioDestination)
+}
+
+function gotStream(stream) {
+  window.stream = stream // make stream available to console
+  videoElement.srcObject = stream
+
+  // record stream
+  var options = { mimeType: 'video/webm;codecs=vp9' }
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.log(options.mimeType + ' is not Supported')
+    options = { mimeType: 'video/webm;codecs=vp8' }
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+      console.log(options.mimeType + ' is not Supported')
+      options = { mimeType: 'video/webm' }
+      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.log(options.mimeType + ' is not Supported')
+        options = { mimeType: '' }
+      }
+    }
+  }
+
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options)
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder: ' + e)
+    alert(
+      'Exception while creating MediaRecorder: ' +
+        e +
+        '. mimeType: ' +
+        options.mimeType
+    )
+    return
+  }
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options)
+
+  mediaRecorder.onstop = handleStop
+  mediaRecorder.ondataavailable = handleDataAvailable
+  mediaRecorder.start(10) // collect 10ms of data
+  console.log('MediaRecorder started', mediaRecorder)
+
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices()
+}
+
+function handleDataAvailable(event) {
+  if (event.data && event.data.size > 0) {
+    recordedBlobs.push(event.data)
+  }
+}
+
+function handleStop(event) {
+  console.log('Recorder stopped: ', event)
+}
+
+setTimeout(function() {
+  var superBuffer = new Blob(recordedBlobs, { type: 'video/webm' })
+  console.log(superBuffer)
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://audio-recorder.72.localhost',
+    contentType: 'application/json; charset=utf-8',
+    data: {
+      type: 'video/webm',
+      blob: '010010'
+    },
+    // dataType: 'json',
+    success: function(data) {
+      console.log(data)
+    }
+  })
+}, 1000)
+
+function start() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop()
+    })
+  }
+  var audioSource = audioInputSelect.value
+  var videoSource = videoSelect.value
+  var constraints = {
+    audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
+    video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+  }
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(gotStream)
+    .then(gotDevices)
+    .catch(handleError)
+}
+
+audioInputSelect.onchange = start
+audioOutputSelect.onchange = changeAudioDestination
+videoSelect.onchange = start
+
+start()
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error)
+}
