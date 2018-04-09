2d7888fd4b7f7da27d185ea17c081ac9ae8454e7
Donny <1021001390@qq.com>
2018-04-09 14:01:22 +0800

    add audio wave

--- src/server/public/js/main.js
+++ src/server/public/js/main.js
@@ -125,6 +125,18 @@ function gotStream(stream) {
   videoElement.srcObject = stream
   videoElement.muted = true
 
+  // visualization stream
+  source = audioCtx.createMediaStreamSource(stream)
+  source.connect(analyser)
+  analyser.connect(distortion)
+  distortion.connect(biquadFilter)
+  biquadFilter.connect(convolver)
+  convolver.connect(gainNode)
+  gainNode.connect(audioCtx.destination)
+
+  visualize()
+  voiceChange()
+
   // Refresh button list in case labels have become available
   return navigator.mediaDevices.enumerateDevices()
 }
@@ -246,4 +258,167 @@ start()
 
 function handleError(error) {
   console.log('navigator.getUserMedia error: ', error)
+}
+
+// set up canvas context for visualizer
+const canvas = document.querySelector('.visualizer')
+const canvasCtx = canvas.getContext("2d")
+
+const intendedWidth = document.querySelector('.wrapper').clientWidth
+canvas.setAttribute('width', intendedWidth)
+
+const visualSelect = document.getElementById("visual")
+
+let drawVisual, WIDTH, HEIGHT
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+const voiceSelect = document.getElementById("voice")
+let source
+
+const analyser = audioCtx.createAnalyser()
+analyser.minDecibels = -90
+analyser.maxDecibels = -10
+analyser.smoothingTimeConstant = 0.85
+
+const distortion = audioCtx.createWaveShaper()
+const gainNode = audioCtx.createGain()
+const biquadFilter = audioCtx.createBiquadFilter()
+const convolver = audioCtx.createConvolver()
+
+function visualize() {
+  WIDTH = canvas.width
+  HEIGHT = canvas.height
+
+  const visualSetting = visualSelect.value
+  console.log(visualSetting)
+
+  if (visualSetting === "sinewave") {
+    analyser.fftSize = 2048
+    const bufferLength = analyser.fftSize
+    console.log(bufferLength)
+    const dataArray = new Uint8Array(bufferLength)
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
+
+    const draw = function () {
+
+      drawVisual = requestAnimationFrame(draw)
+
+      analyser.getByteTimeDomainData(dataArray)
+
+      canvasCtx.fillStyle = 'rgb(200, 200, 200)'
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
+
+      canvasCtx.lineWidth = 2
+      canvasCtx.strokeStyle = 'rgb(0, 0, 0)'
+
+      canvasCtx.beginPath()
+
+      const sliceWidth = WIDTH * 1.0 / bufferLength
+      let x = 0
+
+      for (let i = 0; i < bufferLength; i++) {
+
+        const v = dataArray[i] / 128.0
+        const y = v * HEIGHT / 2
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y)
+        } else {
+          canvasCtx.lineTo(x, y)
+        }
+
+        x += sliceWidth
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height / 2)
+      canvasCtx.stroke()
+    }
+
+    draw()
+
+  } else if (visualSetting === "frequencybars") {
+    analyser.fftSize = 256
+    const bufferLengthAlt = analyser.frequencyBinCount
+    console.log(bufferLengthAlt)
+    const dataArrayAlt = new Uint8Array(bufferLengthAlt)
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
+
+    const drawAlt = function () {
+      drawVisual = requestAnimationFrame(drawAlt)
+
+      analyser.getByteFrequencyData(dataArrayAlt)
+
+      canvasCtx.fillStyle = 'rgb(0, 0, 0)'
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
+
+      const barWidth = (WIDTH / bufferLengthAlt) * 2.5
+      let barHeight
+      let x = 0
+
+      for (let i = 0; i < bufferLengthAlt; i++) {
+        barHeight = dataArrayAlt[i]
+
+        canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)'
+        canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2)
+
+        x += barWidth + 1
+      }
+    }
+
+    drawAlt()
+
+  } else if (visualSetting === "off") {
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
+    canvasCtx.fillStyle = "red"
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
+  }
+
+}
+
+function makeDistortionCurve(amount) {
+  const k = typeof amount === 'number' ? amount : 50,
+    n_samples = 44100,
+    curve = new Float32Array(n_samples),
+    deg = Math.PI / 180
+  let i = 0,
+    x
+  for (; i < n_samples; ++i) {
+    x = i * 2 / n_samples - 1
+    curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x))
+  }
+  return curve
+}
+
+function voiceChange() {
+
+  distortion.oversample = '4x'
+  biquadFilter.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
+  convolver.buffer = undefined
+
+  const voiceSetting = voiceSelect.value
+  console.log(voiceSetting)
+
+  if (voiceSetting === "distortion") {
+    distortion.curve = makeDistortionCurve(400)
+  } else if (voiceSetting === "biquad") {
+    biquadFilter.type = "lowshelf"
+    biquadFilter.frequency.setTargetAtTime(1000, audioCtx.currentTime, 0)
+    biquadFilter.gain.setTargetAtTime(25, audioCtx.currentTime, 0)
+  } else if (voiceSetting === "off") {
+    console.log("Voice settings turned off")
+  }
+
+}
+
+// event listeners to change visualize and voice settings
+
+visualSelect.onchange = function () {
+  window.cancelAnimationFrame(drawVisual)
+  visualize()
+}
+
+voiceSelect.onchange = function () {
+  voiceChange()
 }
